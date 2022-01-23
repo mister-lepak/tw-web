ec43a513d73c3a64b35a8d27081db0e6bf289b02
Try Wijono <try.wijono@99.co>
2022-01-23 12:31:32 +0800

    v1.3: github pages deployment config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
+const debug = process.env.NODE_ENV !== "production";
+
 module.exports = {
   reactStrictMode: true,
-}
+  assetPrefix: !debug ? "/tw-web" : "",
+  images: {
+    loader: "akamai",
+    path: "",
+  },
+};
