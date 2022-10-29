078f4c4201a6ca863035d305d035a07357022581
Try Wijono <try.wijono@99.co>
2022-10-30 01:06:59 +0800

    Deploy Next.js to gh-pages

--- out/_next/static/KPG3FhdOYH86rduIdkyUU/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/6fac0f5a46ea9887.css","static/chunks/pages/index-fc2fbe3ddc2856da.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
